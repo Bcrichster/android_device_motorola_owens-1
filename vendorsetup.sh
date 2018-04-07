e4c143fbda09c2bb1994e8775cc536571daa0ef2
Bcrichster <Bcrichster@users.noreply.github.com>
2018-04-07 19:23:13 -0400

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_owens-userdebug
+#add_lunch_combo lineage_owens-userdebug
 #add_lunch_combo bliss_owens-userdebug
-#add_lunch_combo aokp_owens-userdebug
+add_lunch_combo aokp_owens-userdebug
